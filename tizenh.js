01cae0ac28391ed22a9fc0360bd9443ce26d3994
ormiati <attila.ormos@gmail.com>
2020-11-21 21:34:59 +0100

    13ig

---
+++ tizenh.js
@@ -0,0 +1,37 @@
+'use strict'
+
+console.log('13. feladat:');
+
+const trd=[32, 54, 11, 44, 98, 88, 12];
+
+function bubbleSort (nordered) {
+    let swapped;
+    do {
+        swapped = false;
+        for (let i = 0; i < nordered.length; i +=1) {
+            if (nordered[i] > nordered[i + 1]) {
+                let tmp = nordered[i];
+                nordered[i] = nordered[i + 1];
+                nordered[i + 1] = tmp;
+                swapped = true;
+            }
+        }
+    } while (swapped);
+    console.log(nordered);
+};
+
+    bubbleSort(trd);
+
+    let newnum = document.getElementsByClassName('txt')[0].value;
+
+    function decide(newnum){
+        if(newnum === 'number'){
+            trd.push(newnum);
+            bubbleSort(trd);
+            window.alert('Köszi!')
+        }
+        else {
+            window.alert('Ez nem egész szám!');
+        }
+    }
+
