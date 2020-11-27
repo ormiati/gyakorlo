e099c7976c0382ccdba84e9209741b4358e22ba7
ormiati <attila.ormos@gmail.com>
2020-11-27 21:11:25 +0100

    szöveg 2. feladat

--- szoveg.js
+++ szoveg.js
@@ -3,7 +3,7 @@ console.log('Szövegek')
 console.log('1.feladat:');
 
 const johnDoe = { 
-    firstName: 'Jonh',
+    firstName: 'John',
     lastName: 'Doe'
 };
 
@@ -18,4 +18,10 @@ console.log('0:',merge[0],'1:', merge[1]);
 
 console.log('2. feladat:');
 
+const tag=(texts, ...values)=> 
+texts.map((text, index)=>
+`${text.italics()} ${values[index] ? `${values[index].bold()}` : ''}` 
+).join('');
 
+const sent = tag `My name is ${johnDoe.firstName} ${johnDoe.lastName}, and this is my sister ${janeDoe.firstName} ${janeDoe.lastName}`;
+document.body.innerHTML = sent;
